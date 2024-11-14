05e2aec0e552249e4ff795032f18427f12216162
Yende Mangum <yendemangum@gmail.com>
2024-11-14 12:01:10 -0500

    Edit navbar links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,17 +8,13 @@ function Navbar() {
         <nav className={navbar.nav}>
           <Link to="/">
             <img
-              src="/public/D2FA6g.svg"
+              src="../../public/D2FA6g.svg"
               alt="Yende Mangum logo"
               className={navbar.icon}
             />
           </Link>
-          <Link to="/">
-            <span className={navbar.link}>Home</span>
-          </Link>
-          <Link to="/lessons">
-            <span className={navbar.link}>Lessons</span>
-          </Link>
+          <Link to="/" className={navbar.link}>Home</Link>{" "}
+          <Link to="/lessons" className={navbar.link}>Lessons</Link>
         </nav>
       </header>
     </>
